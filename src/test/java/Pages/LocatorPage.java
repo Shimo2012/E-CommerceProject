e9c1de85c675df68c2be909c705a04244ddbf503
shaym <shaymaa2012@gmail.com>
2024-02-12 10:22:20 -0500

    ParentClass is created

--- src/test/java/Pages/LocatorPage.java
+++ src/test/java/Pages/LocatorPage.java
@@ -1,4 +1,13 @@
 package Pages;
 
-public class LocatorPage {
+import Utilities.BaseDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class LocatorPage extends BaseDriver {
+    public List<WebElement> getSubtabs() {
+        List<WebElement> subtabs = null;
+        return subtabs;
+    }
 }
